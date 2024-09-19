32f1b7cbb99e95eaa32596a999314af32f51f7d2
deepa robisina <robisina.deepa@bracits.com>
2024-09-19 11:58:14 +0600

    done 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 9001
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
 
 
 // middle ware
@@ -27,16 +27,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    app.get('/coffees', async (req, res) => {
+      console.log("Coffees Loading....")
+      // const cursor = userCollection.find();
+      // const result = await cursor.toArray();
+      // res.send(result);
+    });
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      // const result = await coffeeCollection.insertOne(newCoffee);
+      // res.send(result);
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-// run().catch(console.dir);
-
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Coffee making server is running')
+})
 
 app.listen(port, ()=> {
   console.log("app listening running successfully")
